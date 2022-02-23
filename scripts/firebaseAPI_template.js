f1adb9b77334fabf8142d9b723344ff78c1f7bd2
bhironaka1 <bhironaka1@my.bcit.ca>
2022-02-22 22:34:04 -0700

    Finish and submit
    finished demo

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBIQS1vw-cOwmY0599wEkFsFTWEuW3QjZA",
+    authDomain: "fir-comp1800-cb8d4.firebaseapp.com",
+    projectId: "fir-comp1800-cb8d4",
+    storageBucket: "fir-comp1800-cb8d4.appspot.com",
+    messagingSenderId: "930495559224",
+    appId: "1:930495559224:web:35f3f7f0c7bee0808758cd",
+    measurementId: "G-C0J85NVT46"
 };
 
 //--------------------------------------------
